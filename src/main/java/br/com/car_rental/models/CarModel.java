592761b3bca4b47540dde3f5c822772ed2896063
Danilo Diniz <danilodiniz93@yahoo.com>
2024-10-10 00:20:55 -0300

    Docker file created

--- src/main/java/br/com/car_rental/models/CarModel.java
+++ src/main/java/br/com/car_rental/models/CarModel.java
@@ -1,19 +1,17 @@
 package br.com.car_rental.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Entity
 @Data
+@Table(name = "car")
 public class CarModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private UUID id;
     private String name;
-    private String carModel;
+    private int year;
 }
